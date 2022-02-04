36944ee370025e652030ef623bece2ad8fbea45a
thatFemicode <leyedavid10@gmail.com>
2022-02-04 23:42:40 +0100

    fixed custom cursor

--- src/Components/CustomCursor/CustomCursorStyled.js
+++ src/Components/CustomCursor/CustomCursorStyled.js
@@ -8,7 +8,7 @@ export const CustomCursorStyled = styled.div`
     transform: translate3d(0, 0, 0);
     position: fixed;
     background-image: transparent;
-    border: 3px solid black;
+    border: 3px solid #000;
     transition: width 0.5s cubic-bezier(0.19, 1, 0.22, 1),
       height 0.5s cubic-bezier(0.19, 1, 0.22, 1);
     transform-origin: center;
